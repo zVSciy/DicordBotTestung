34bcaea0f82dea1871fe54a0f65c2ef51b2e024a
zVSciy <manager.fallen0815@gmail.com>
2023-10-17 15:44:04 +0200

    Update createchannel.js

--- commands/admin/createchannel.js
+++ commands/admin/createchannel.js
@@ -1,21 +1,107 @@
-const { SlashCommandBuilder } = require('discord.js');
+// const { SlashCommandBuilder } = require('discord.js');
+
+// module.exports = {
+//     data: new SlashCommandBuilder()
+//         .setName('createchannel')
+//         .setDescription('creates a channel')
+//         .addChannelOption(option => option
+//             .setName('channelname')
+//             .setRequired(true),
+//         ),
+//     async execute(interaction) {
+//         const channel = interaction.option.getChannel('channel');
+
+//         try {
+//             await interaction.create.channel(channel.name);
+//         } catch (error) {
+//             console.error(error);
+//         }
+
+//     },
+// };
+
+const {SlashCommandBuilder, PermissionFlagsBits, ChannelType, GuildCategory} = require ('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('createchannel')
-        .setDescription('creates a channel')
-        .addChannelOption(option => option
-            .setName('channelname')
-            .setRequired(true),
-        ),
+    .setName("create-channel")
+    .setDescription("Create a custom channel")
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels)
+    .addStringOption(option =>
+    option.setName("channeltype")
+    .setRequired(true)
+    .setDescription("Set the type of the channel")
+    .addChoices(
+        { name: "Text channel", value: "textchannel"}
+        { name: "Voice channel", value: "voicechannel"}
+    )
+    )
+    .addStringOption(option =>
+    option.setName("channelname")
+    .setDescription("Set the parent of the channel.")
+    .setRequired(true)
+    .addChannelTypes(ChannelType.GuildCategory)
+    )
+    .addRoleOption(option =>
+    option.setName("permission-role")
+    .setDescription("The permission role for the server")
+    .setRequired(true)    
+    )
+    .addRoleOption(option =>
+    option.setName("everyone")
+    .setDescription("@everyone")
+    .setRequired(true)    
+    ),
     async execute(interaction) {
-        const channel = interaction.option.getChannel('channel');
+        const {guild, member, options } = interaction;
 
-        try {
-            await interaction.create.channel(channel.name);
-        } catch (error) {
-            console.error(error);
+        const {Viewchannel, ReadMessageHistory, SendMessages, Connect, Speak } = PermissionFlagsBits;
+
+        const channeltype = options.getString("channeltype")
+        const channelname = options.getString("channelname")
+        const parent = options.getChannel("parent")
+        const permissions = options.getRole("permission-role")
+        const everyone = options.getRole("everyone")
+
+        if (channeltype === "textchannel") {
+            await guild.channels.create({
+                name: `${channelname}`,
+                type: ChannelType.GuildText,
+                parent: parent,
+
+                permissionsOverwrites: [
+                    {
+                        id: permissions
+                        allow: [Viewchannel, SendMessages, ReadMessageHistory],
+                    },
+                    {
+                        id: everyone,
+                        deny: [Viewchannel, SendMessages, ReadMessageHistory],
+                    }
+                ]
+            })
         }
 
-    },
-};
+        if (channeltype === "voicechannel") {
+            await guild.channels.create({
+                name: `${channelname}`,
+                type: ChannelType.GuildText,
+                parent: parent,
+
+                permissionsOverwrites: [
+                    {
+                        id: permissions
+                        allow: [Viewchannel, SendMessages, ReadMessageHistory],
+                    },
+                    {
+                        id: everyone,
+                        deny: [Viewchannel, SendMessages, ReadMessageHistory],
+                    }
+                ]
+            })
+        }
+
+    }
+
+    
+}
