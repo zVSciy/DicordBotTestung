a08ed5de63f7c3c2bda7fb30ed638ecde412b8d2
KaveXD <KaveXD@gmail.com>
2023-10-17 17:25:27 +0200

    mute command actually works now

--- commands/admin/mute.js
+++ commands/admin/mute.js
@@ -16,18 +16,19 @@ module.exports = {
         .setRequired(true),
         ),
     async execute(interaction) {
+        const guild = interaction.guild;
         const target = interaction.options.getUser('user');
+        const member = guild.members.cache.get(interaction.options.getUser('user').id);
         const bool = interaction.options.getBoolean('mute');
 
         const db = new dataBaseAccess('./db.sqlite3');
 
         if (bool) {
             db.writeUserToTable('mutedUsers', target.id);
+            await member.voice.setMute(true);
         } else {
             db.removeUserFromTable('mutedUsers', target.id);
+            await member.voice.setMute(false);
         }
-
-
-
     },
 };
