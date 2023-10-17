00e47ede3842b28d5db726ae982e921da3c7a3e1
KaveXD <KaveXD@gmail.com>
2023-10-17 14:47:46 +0200

    added databaseaccess file

---
+++ databaseAccess.js
@@ -0,0 +1,11 @@
+class klasse {
+    constructor(name, amongus) {
+        this.name = name;
+        this.amongus = amongus;
+    }
+    test() {
+        console.log('testies');
+    }
+}
+
+module.exports = klasse;
