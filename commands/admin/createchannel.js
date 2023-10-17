f5a762b115cf40f71b5334c057dbe9b9d9a13d1b
zVSciy <manager.fallen0815@gmail.com>
2023-10-17 16:56:27 +0200

    Update createchannel.js

--- commands/admin/createchannel.js
+++ commands/admin/createchannel.js
@@ -1,25 +1,3 @@
-// const { SlashCommandBuilder } = require('discord.js');
-
-// module.exports = {
-//     data: new SlashCommandBuilder()
-//         .setName('createchannel')
-//         .setDescription('creates a channel')
-//         .addChannelOption(option => option
-//             .setName('channelname')
-//             .setRequired(true),
-//         ),
-//     async execute(interaction) {
-//         const channel = interaction.option.getChannel('channel');
-
-//         try {
-//             await interaction.create.channel(channel.name);
-//         } catch (error) {
-//             console.error(error);
-//         }
-
-//     },
-// };
-
 const {SlashCommandBuilder, PermissionFlagsBits, ChannelType, GuildCategory} = require ('discord.js');
 
 module.exports = {
